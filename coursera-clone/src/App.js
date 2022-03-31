256606c205c059ad3d658961f9cfb90b3814858d
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-03-31 14:33:38 +0530

    second box initiated

--- coursera-clone/src/App.js
+++ coursera-clone/src/App.js
@@ -1,11 +1,14 @@
 import Firstbox from "./components/Firstbox/Firstbox";
+import Secondbox from "./components/Secondbox/Secondbox";
+import React from "react";
 // import './App.css';
-
 
 function App() {
   return (
     <div className="App">
-      <Firstbox/>
+      <Firstbox />
+      <Secondbox />
+
     </div>
   );
 }
